61857fa8a6696d5e4ade7147bc467a4743e34797
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-01-15 12:18:45 +0000

    creating the playlist service

--- src/playlists/playlists.controller.ts
+++ src/playlists/playlists.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get,  Param, Post} from '@nestjs/common';
 
 @Controller('playlists')
-export class PlaylistsController {}
+export class PlaylistsController {
+
+    @Get()
+    findAll() {
+        return "This are playlists";
+    }
+
+    @Get(":id")
+    findOne(@Param() id) {
+        return `This is a playlist ${id}`;
+    }
+
+    @Post()
+    create() {
+        return "New playlist created";
+    }
+}
+
+
