20e39a0efa6947b98dc6bd3db6277193ab1f2b53
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-02-11 17:35:39 +0000

    refactor: remove Spotify authentication module and related files

--- src/spotify-auth/spotify-auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpotifyAuthController } from './spotify-auth.controller';
-import { SpotifyAuthService } from './spotify-auth.service';
-
-@Module({
-    controllers: [SpotifyAuthController],
-    providers: [SpotifyAuthService],
-})
-export class SpotifyAuthModule {}
