d78484347f5913baa2ecde69e374473f899c0894
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-02-11 17:36:35 +0000

    refactor: remove OAuth module and service files

--- src/oauth/oauth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OauthService } from './oauth.service';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Module({
-  providers: [OauthService, PrismaService],
-  exports: [OauthService],
-})
-export class OauthModule {}
